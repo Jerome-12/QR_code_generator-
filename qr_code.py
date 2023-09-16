38f44abbde8fafe46dc3f425d5845c858b83af6b
Jerome-12 <140602821+Jerome-12@users.noreply.github.com>
2023-09-16 12:06:16 +0530

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,11 +1,8 @@
-from PIL import Image
 import qrcode
 
 text=input('Enter text : ')
 
 qr=qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
     border=3,
 )
@@ -16,3 +13,5 @@ qr.make(fit=True)
 img=qr.make_image(fill_color="black",back_color="white")
 
 img.save('qr.png')
+
+print("Qr code generated")
